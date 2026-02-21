0b2c3b68676160851e6fa27d5449ef70d22922c4
lxl <lixiaolongcuit@gmail.com>
2026-02-21 14:45:43 +0800

    Initial Commit

---
+++ OneStep/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OneStep
+//
+//  Created by lixiaolong on 2026/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OneStep/OneStepApp.swift
@@ -0,0 +1,17 @@
+//
+//  OneStepApp.swift
+//  OneStep
+//
+//  Created by lixiaolong on 2026/2/21.
+//
+
+import SwiftUI
+
+@main
+struct OneStepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
